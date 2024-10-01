261701eef87bbb271b0877d3eeec7d2becb8bbe1
fardanufibri25 <fardanufibri25@gmail.com>
2024-10-01 12:49:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ private:
     int umur;
     float ipk;
 
+Public:
+    Mahasiswa(string n, int u, float i) : nama(n), umur(u), ipk(i) {}
+
+    Void displayInfo() const {
+        Cout << “Nama: “ << nama << “, Umur: “ << umur << “, IPK: “ << ipk << endl;
+    }
+
+     Bool isLulus() const {
+Return umur >= 18 && ipk >= 3.0;
+    }
+
+
 
 
 
